4c49ed7ea25a893bde2c9608b33ba029a170a79a
Rodrigo DAmbrosio <rodrigo.dambrosio@gmail.com>
2024-10-11 20:17:36 -0300

    Add cosas para pila

---
+++ pila.h
@@ -0,0 +1,21 @@
+#ifndef PILA_H
+#define PILA_H
+
+typedef struct Nodo{
+    void* dato;
+    struct Nodo* sig;
+}Nodo;
+
+typedef struct pila
+{
+    Nodo* tope;
+}Pila;
+
+Pila* crear_pila();
+void apilar(Pila* pila, void* dato, size_t tamano);
+void* desapilar(Pila* pila);
+int check_esta_vacia(Pila* pila);
+void destruir_pila(Pila* pila);
+
+#endif
+
